c4e948ba4ba25b0e8299415c6c3d4fcaf8b80c45
John Kent Reynes <johnkentreynes@gmail.com>
2015-10-05 08:14:09 +0800

    refactor(): change the coding styles

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ var handlebars = require( 'handlebars' );
 var glob       = Promise.promisify( require( 'glob' ) );
 var _          = require( 'lodash' );
 var fs         = require( 'fs' );
-var jsonFiles  = [];
-var templates  = [];
-var config;
 
-function generateName ( path, type ) {
+SpecGenerator.prototype.generateName = function ( path, type ) {
 
 	if ( type === 'partial' ) {
 		var start =  _.findIndex( path, function ( element, index ) {
@@ -21,24 +18,28 @@ function generateName ( path, type ) {
 				return index;
 			}
 		} ) - 1;
-		return _.camelCase( path[ start ] + '-' +path[ end ] + '-' + ( path[ path.length - 1 ].split( '.' )[ 0 ] ) + 'Partial' ) ;
+		return _.camelCase(
+			[
+				path[ start ],
+				'-',
+				path[ end ],
+				'-',
+				( path[ path.length - 1 ].split( '.' )[ 0 ] ),
+			 	'Partial'
+			].join('')
+		);
 	}
-
-	else {
 		return path.substring( path.lastIndexOf('/') + 1, path.length - 3 );
-	}
-
 }
 
-function getJSONFiles () {
-	return glob( config.ddJsonFiles + '/**/*.json' )
+SpecGenerator.prototype.getJSONFiles = function ( path ) {
+	return glob( path + '/**/*.json' )
 		.then( function ( files ) {
-			jsonFiles = files;
-			return;
+			return files;
 		} );
 }
 
-function getTemplate ( scenario ) {
+SpecGenerator.prototype.getTemplate = function ( templates, scenario ) {
 	return _.find( templates, function ( element ) {
 		if( generateName( element, 'template' ) === scenario ) {
 			return element;
@@ -46,16 +47,16 @@ function getTemplate ( scenario ) {
 	} );
 }
 
-function getTemplates() {
-	  return glob( config.ddTemplates + '/**/*.js', { ignore : config.ddTemplates + '/**/partials/**/*.js' } )
+SpecGenerator.prototype.getTemplates = function ( path ) {
+	var options =  { ignore : path + '/**/partials/**/*.js' };
+	  return glob( path + '/**/*.js', options )
 		.then( function ( files, err ) {
-			templates = files;
-			return;
+			return files;
 		} );
 }
 
-function registerPartials () {
-	return glob( config.ddTemplates + '/**/partials/**/*.js' )
+SpecGenerator.prototype.registerPartials = function () {
+	return glob( path + '/**/partials/**/*.js' )
 		.then ( function ( files, err ) {
 			_.forEach( files, function ( element ) {
 				handlebars.registerPartial( generateName( element.split( '/' ), 'partial' ), fs.readFileSync( element, 'utf8' ) );
@@ -64,36 +65,57 @@ function registerPartials () {
 		} );
 }
 
-function initialize () {
-	return getJSONFiles()
-		.then( getTemplates )
-		.then( registerPartials );
-}
-
-module.exports = function ( options ) {
+function SpecGenerator ( options ) {
+	var jsonFiles  = [];
+	var templates  = [];
 	var spec;
 	var jsonData;
 
-	config = options;
+	var self = this;
 
-	initialize()
+	this.getJSONFiles( options.ddJsonFiles )
+		.then( function ( jsonFiles ) {
+			jsonFiles = jsonFiles;
+			return self.getTemplates ( options.ddTemplates );
+		} )
+		.then( function ( templates ) {
+			templates = files;
+			registerPartials ( options.ddTemplates )
+		} )
 		.then( function () {
 			require( './node_modules/handlebars-helpers/lib/helper-lib.js' ).register( handlebars, {} );
 
 			_.forEach( jsonFiles, function ( element, index ) {
 
-				jsonData = require( process.cwd() + element.substr(1,element.length) );
-				spec = handlebars.compile( fs.readFileSync( getTemplate ( jsonData.scenario ), 'utf8' ) );
+				jsonData = require( process.cwd() + element.substr( 1, element.length ) );
+				spec = handlebars.compile( fs.readFileSync( this.getTemplate ( templates, jsonData.scenario ), 'utf8' ) );
 
-				try {
-					fs.mkdirSync( config.ddGeneratedSpecs + '/' + jsonData.scenario );
-					fs.mkdirSync( config.ddGeneratedSpecs + '/' + jsonData.scenario + '/' + jsonData.scenarioType );
-				}
-				catch( err ){
-					console.log( err );
-				}
+				var parentPath = [
+									options.ddGeneratedSpecs,
+									'/',
+									jsonData.scenario,
+									'/'
+								].join('');
 
-				fs.writeFileSync( config.ddGeneratedSpecs + '/' +  jsonData.scenario + '/' + jsonData.scenarioType + '/' + jsonData.scenario + jsonData.scenarioId + '.spec.js', spec( jsonData ) );
+				var childPath = [
+									path,
+									jsonData.scenarioType,
+									'/'
+									].join( '' );
+				var generatedSpecName = [
+											jsonData.scenario,
+											jsonData.scenarioId,
+											'.spec.js'
+										].join( '' );
+
+				fs.mkdirSync( parentPath );
+				fs.mkdirSync( childPath );
+				fs.writeFileSync( childPath + generatedSpecName, spec( jsonData ) );
 			} );
+		} )
+		.catch( function ( err ) {
+			console.log( err );
 		} );
 }
+
+module.exports = SpecGenerator;
---
+++ test.js
@@ -0,0 +1,12 @@
+var test = require( './index.js' );
+
+( function () {
+		var test1 = new test( {
+			"ddJsonFile"      : "./test/dd-e2e/json-file/",
+			"ddTemplates"     : "./test/dd-e2e/spec-template/",
+			"ddGeneratedSpec" : "./test/dd-e2e/generated-spec/"
+
+	} );
+} )();
+
+
